a3837d3383e88b5663c0b490ae3dab5d2ee0dec0
DESKTOP-S59BJJM\personal <shahzadahamad285@gmail.com>
2024-09-05 12:05:32 +0530

    Final changes for working Consistent hashing load balancer

---
+++ LoadBalancerWithConsistentHashingProtoType/LoadBalancer.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoadBalancerWithConsistentHashingProtoType
+{
+    public class LoadBalancer
+    {
+        private SortedDictionary<BigInteger, Server> hashRing = new SortedDictionary<BigInteger, Server>();
+        private int numberOfReplicas;
+
+        public LoadBalancer(int numberOfReplicas = 0)
+        {
+            this.numberOfReplicas = numberOfReplicas;
+        }
+
+        private BigInteger Hash(string input)
+        {
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                byte[] hashBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(input));
+                return new BigInteger(hashBytes);
+            }
+        }
+
+        public void AddServer(Server server)
+        {
+            numberOfReplicas++;
+            string replicaId = $"{server._port}";
+            var position = Hash(replicaId);
+            hashRing[position] = server;
+            Console.WriteLine($"Server on port {server._port} added to hash ring at position {position}");
+
+        }
+
+        public Server GetServer(string key)
+        {
+            var keyHash = Hash(key);
+
+            foreach (var position in hashRing.Keys)
+            {
+                if (keyHash <= position)
+                {
+                    return hashRing[position];
+                }
+            }
+
+            return hashRing[hashRing.First().Key]; // Wrap around to the first server
+        }
+
+        public void RouteRequest(string request)
+        {
+            var server = GetServer(request);
+            Console.WriteLine($"Number of Servers {numberOfReplicas} Routing request '{request}' to server on port {server._port}");
+        }
+    }
+}
---
+++ LoadBalancerWithConsistentHashingProtoType/Program.cs
@@ -0,0 +1,60 @@
+﻿
+
+using LoadBalancerWithConsistentHashingProtoType;
+
+LoadBalancer loadBalancer = new LoadBalancer();
+
+// Create and start initial servers on different ports
+Server server1 = new Server(5001);
+Server server2 = new Server(5002);
+Server server3 = new Server(5003);
+
+loadBalancer.AddServer(server1);
+loadBalancer.AddServer(server2);
+loadBalancer.AddServer(server3);
+
+// Start servers on separate threads
+StartServerThread(server1);
+StartServerThread(server2);
+StartServerThread(server3);
+
+
+Thread requestThread = new Thread(() =>
+{
+    int i = 1;
+    while (true)
+    {
+        
+            loadBalancer.RouteRequest("Req"+i);
+            Thread.Sleep(3000); // Simulate time between requests
+        i++;
+    }
+});
+requestThread.Start();
+
+// Listen for input to add a new server
+while (true)
+{
+    Console.WriteLine("Enter a port number to add a new server, or 'q' to quit:");
+    var input = Console.ReadLine();
+
+    if (input.ToLower() == "q")
+        break;
+
+    if (int.TryParse(input, out int newPort))
+    {
+        Server newServer = new Server(newPort);
+        loadBalancer.AddServer(newServer);
+        StartServerThread(newServer);
+    }
+    else
+    {
+        Console.WriteLine("Invalid port number. Please try again.");
+    }
+}
+
+static void StartServerThread(Server server)
+{
+    Thread serverThread = new Thread(new ThreadStart(server.Start));
+    serverThread.Start();
+}
